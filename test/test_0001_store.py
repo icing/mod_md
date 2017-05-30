dc93440e5c999a80a544dd46dca7bd04db87fb38
Michael Köller <michael.koeller@greenbytes.de>
2017-05-30 09:30:55 +0200

    add tests: a2dm store

--- test/test_0001_store.py
+++ test/test_0001_store.py
@@ -23,93 +23,153 @@ ACME_URL = config.get('acme', 'url')
 WEBROOT = config.get('global', 'server_dir')
 STORE_DIR = os.path.join(WEBROOT, 'md') 
 
-def exec_sub(args):
+def exec_sub_err(args, errCode):
     print "execute: ", " ".join(args)
     p = subprocess.Popen(args, stdout=subprocess.PIPE)
     (outdata, errdata) = p.communicate()
-    assert p.wait() == 0
-    print "result:  ", outdata
+    assert p.wait() == errCode
+    print "result:  (", errCode, ") ", outdata
     return outdata
 
-def check_live(url, timeout):
-    server = urlparse(url)
-    try_until = time.time() + timeout
-    while time.time() < try_until:
-        try:
-            c = HTTPConnection(server.hostname, server.port, timeout=timeout)
-            c.request('HEAD', server.path)
-            resp = c.getresponse()
-            print "response %d %s" % (resp.status, resp.reason)
-            c.close()
-            return True
-        except IOError:
-            print "connect error:", sys.exc_info()[0]
-            time.sleep(.1)
-        except:
-            print "Unexpected error:", sys.exc_info()[0]
-    print "Unable to contact server after %d sec" % timeout
-    return False
+def exec_sub(args):
+    return exec_sub_err(args, 0)
 
 def setup_module(module):
-    print("reset store dir %s" % STORE_DIR)
-    assert len(STORE_DIR) > 1
-    shutil.rmtree(STORE_DIR, ignore_errors=True)
-    os.makedirs(STORE_DIR)
+    print("setup_module: %s" % module.__name__)
         
-    
 def teardown_module(module):
-    print("teardown_module module:%s" % module.__name__)
+    print("teardown_module: %s" % module.__name__)
+
 
 class TestStore:
 
-    def test_env_001(self):
+    def setup_method(self, method):
+        print("setup_method: %s" % method.__name__)
+        # wipe store directory
+        print("clear store dir: %s" % STORE_DIR)
+        assert len(STORE_DIR) > 1
+        shutil.rmtree(STORE_DIR, ignore_errors=True)
+        os.makedirs(STORE_DIR)
+ 
+    def teardown_method(self, method):
+        print("teardown_method: %s" % method.__name__)
+
+
+    def test_001(self):
         # verify expected binary version
         args = [A2MD, "-V"]
         outdata = exec_sub(args)
         m = re.match("version: %s-git$" % config.get('global', 'a2md_version'), outdata)
         assert m
 
-    def test_001(self):
-        # try add a single dns managed domain
-        args = [A2MD, "-a", ACME_URL, "-d", STORE_DIR, "-j" ]
-        dns1 = "greenbytes.de"
-        args.extend([ "store", "add", dns1 ])
-        outdata = exec_sub(args)
-        jout = json.loads(outdata)
-        md = jout['output'][0]
-        assert md['name'] == dns1
-        assert len(md['domains']) == 1 
-        assert md['domains'][0] == dns1
-        assert md['ca']['url'] == ACME_URL
-        assert md['ca']['proto'] == 'ACME'
+    # --------- store add ---------
 
-    def test_002(self):
-        # try add > 1 dns managed domain
-        args = [A2MD, "-a", ACME_URL, "-d", STORE_DIR, "-j" ]
-        dns1 = "greenbytes2.de"
-        dns2 = "www.greenbytes2.de"
-        dns3 = "mail.greenbytes2.de"
-        args.extend([ "store", "add", dns1, dns2, dns3 ])
-        outdata = exec_sub(args)
-        jout = json.loads(outdata)
-        md = jout['output'][0]
-        assert md['name'] == dns1
-        assert len(md['domains']) == 3 
-        assert md['domains'][0] == dns1
-        assert md['domains'][1] == dns2
-        assert md['domains'][2] == dns3
-        assert md['ca']['url'] == ACME_URL
-        assert md['ca']['proto'] == 'ACME'
-
+    # test: add a single dns managed domain
     def test_100(self):
-        # create and remove a managed domain
         args = [A2MD, "-a", ACME_URL, "-d", STORE_DIR, "-j" ]
-        dns1 = "test-100.com"
-        args.extend([ "store", "add", dns1 ])
+        dns = "greenbytes.de"
+        args.extend([ "store", "add", dns ])
         outdata = exec_sub(args)
+        jout = json.loads(outdata)
+        md = jout['output'][0]
+        assert md['name'] == dns
+        assert len(md['domains']) == 1 
+        assert md['domains'][0] == dns
+        assert md['ca']['url'] == ACME_URL
+        assert md['ca']['proto'] == 'ACME'
+
+    # test: add > 1 dns managed domain
+    def test_101(self):
         args = [A2MD, "-a", ACME_URL, "-d", STORE_DIR, "-j" ]
-        dns1 = "test-100.com"
-        args.extend([ "store", "remove", dns1 ])
+        dns = [ "greenbytes2.de", "www.greenbytes2.de", "mail.greenbytes2.de" ]
+        args.extend([ "store", "add" ])
+        args.extend(dns)
+        outdata = exec_sub(args)
+        jout = json.loads(outdata)
+        md = jout['output'][0]
+        assert md['name'] == dns[0]
+        assert len(md['domains']) == 3 
+        assert md['domains'] == dns
+        assert md['ca']['url'] == ACME_URL
+        assert md['ca']['proto'] == 'ACME'
+
+    # test: add second managed domain
+    def test_102(self):
+        # setup: add first managed domain
+        args = [A2MD, "-a", ACME_URL, "-d", STORE_DIR, "-j" ]
+        dns1 = [ "test-100.com", "test-101.com", "test-102.com" ]
+        args.extend([ "store", "add" ])
+        args.extend(dns1)
+        outdata = exec_sub(args)
+        # add second managed domain
+        args = [A2MD, "-a", ACME_URL, "-d", STORE_DIR, "-j" ]
+        dns2 = [ "greenbytes2.de", "www.greenbytes2.de", "mail.greenbytes2.de" ]
+        args.extend([ "store", "add" ])
+        args.extend(dns2)
+        outdata = exec_sub(args)
+        jout = json.loads(outdata)
+        assert len(jout['output']) == 1
+        md = jout['output'][0]
+        assert md['name'] == dns2[0]
+        assert md['domains'] == dns2
+        assert md['ca']['url'] == ACME_URL
+        assert md['ca']['proto'] == 'ACME'
+
+    # --------- store remove ---------
+
+    # test: remove managed domain
+    def test_200(self):
+        # setup: store managed domain
+        args = [A2MD, "-a", ACME_URL, "-d", STORE_DIR, "-j" ]
+        dns = "test-100.com"
+        args.extend([ "store", "add", dns ])
+        outdata = exec_sub(args)
+        # remove managed domain
+        args = [A2MD, "-a", ACME_URL, "-d", STORE_DIR, "-j" ]
+        args.extend([ "store", "remove", dns ])
         outdata = exec_sub(args)
         jout = json.loads(outdata)
         assert 'output' not in jout
+        assert jout['status'] == 0
+
+    # test: remove from list of managed domains 
+    def test_201(self):
+        # setup: add several managed domains
+        args = [A2MD, "-a", ACME_URL, "-d", STORE_DIR, "-j" ]
+        dns1 = [ "test-100.com", "test-101.com", "test-102.com" ]
+        args.extend([ "store", "add"])
+        args.extend(dns1)
+        exec_sub(args)
+        args = [A2MD, "-a", ACME_URL, "-d", STORE_DIR, "-j" ]
+        dns2 = [ "greenbytes2.de", "www.greenbytes2.de", "mail.greenbytes2.de" ]
+        args.extend([ "store", "add" ])
+        args.extend(dns2)
+        outdata = exec_sub(args)
+        # remove managed domain
+        args = [A2MD, "-a", ACME_URL, "-d", STORE_DIR, "-j" ]
+        args.extend([ "store", "remove", "test-100.com" ])
+        outdata = exec_sub(args)
+        jout = json.loads(outdata)
+        assert 'output' not in jout
+        assert jout['status'] == 0
+
+    # test: remove nonexisting managed domain
+    def test_202(self):
+	    # 1st try: error - not found
+        args = [A2MD, "-a", ACME_URL, "-d", STORE_DIR, "-j" ]
+        dns1 = "test-100.com"
+        args.extend([ "store", "remove", dns1 ])
+        outdata = exec_sub_err(args, 1)
+        jout = json.loads(outdata)
+        assert 'output' not in jout
+        assert jout['status'] == 2
+
+    # test: force remove nonexisting managed domain
+    def test_203(self):
+        args = [A2MD, "-a", ACME_URL, "-d", STORE_DIR, "-j" ]
+        dns1 = "test-100.com"
+        args.extend([ "store", "remove", "-f", dns1 ])
+        outdata = exec_sub(args)
+        jout = json.loads(outdata)
+        assert 'output' not in jout
+        assert jout['status'] == 0
