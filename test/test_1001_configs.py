ead23dcdcaf123121afb14b525139a71df92f1f5
Michael Köller <michael.koeller@greenbytes.de>
2017-06-19 15:54:05 +0200

    add acme tests / add comments in apache configs

--- test/test_1001_configs.py
+++ test/test_1001_configs.py
@@ -30,17 +30,15 @@ def teardown_module(module):
 
 class TestConf:
 
-    def new_errors(self):
-        (errors, warnings) = TestEnv.apache_err_count()
-        return errors - self.errors
-        
-    def new_warnings(self):
-        (errors, warnings) = TestEnv.apache_err_count()
-        return warnings - self.warnings
-        
     def setup_method(self, method):
+        print("setup_method: %s" % method.__name__)
         (self.errors, self.warnings) = TestEnv.apache_err_count()
-                            
+
+    def teardown_method(self, method):
+        print("teardown_method: %s" % method.__name__)
+
+    # --------- tests ---------
+
     def test_001(self):
         # just one ManagedDomain definition
         assert TestEnv.apachectl("test_001", "graceful") == 0
@@ -55,62 +53,71 @@ class TestConf:
         # two ManagedDomain definitions, exactly the same
         assert TestEnv.apachectl("test_003", "graceful") == 0
         assert not TestEnv.is_live(TestEnv.HTTPD_URL, .5)
-        assert self.new_errors() == 1
+        assert self._new_errors() == 1
         
     def test_004(self):
         # two ManagedDomain definitions, overlapping
         assert TestEnv.apachectl("test_004", "graceful") == 0
         assert not TestEnv.is_live(TestEnv.HTTPD_URL, .5)
-        assert self.new_errors() == 1
+        assert self._new_errors() == 1
 
     def test_005(self):
-        # two ManagedDomain, one inside a virtual host
+        # two ManagedDomains, one inside a virtual host
         assert TestEnv.apachectl("test_005", "graceful") == 0
         assert TestEnv.is_live(TestEnv.HTTPD_URL, 1)
-        assert self.new_errors() == 0
+        assert self._new_errors() == 0
 
     def test_006(self):
-        # two ManagedDomain, one correct vhost name
+        # two ManagedDomains, one correct vhost name
         assert TestEnv.apachectl("test_006", "graceful") == 0
         assert TestEnv.is_live(TestEnv.HTTPD_URL, 1)
-        assert self.new_errors() == 0
+        assert self._new_errors() == 0
 
     def test_007(self):
-        # two ManagedDomain, two correct vhost names
+        # two ManagedDomains, two correct vhost names
         assert TestEnv.apachectl("test_007", "graceful") == 0
         assert TestEnv.is_live(TestEnv.HTTPD_URL, 1)
-        assert self.new_errors() == 0
+        assert self._new_errors() == 0
 
     def test_008(self):
-        # two ManagedDomain, overlapping vhosts
+        # two ManagedDomains, overlapping vhosts
         assert TestEnv.apachectl("test_008", "graceful") == 0
         assert TestEnv.is_live(TestEnv.HTTPD_URL, 1)
-        assert self.new_errors() == 0
+        assert self._new_errors() == 0
 
     def test_009(self):
         # vhosts with overlapping MDs
         assert TestEnv.apachectl("test_009", "graceful") == 0
         assert not TestEnv.is_live(TestEnv.HTTPD_URL, 1)
-        assert self.new_errors() == 3
+        assert self._new_errors() == 3
 
     def test_010(self):
         # ManagedDomain, vhost with matching ServerAlias
         assert TestEnv.apachectl("test_010", "graceful") == 0
         assert TestEnv.is_live(TestEnv.HTTPD_URL, 1)
-        assert self.new_errors() == 0
-        assert self.new_warnings() == 0
+        assert self._new_errors() == 0
+        assert self._new_warnings() == 0
 
     def test_011(self):
-        # ManagedDomain does misses one ServerAlias
+        # ManagedDomain, misses one ServerAlias
         assert TestEnv.apachectl("test_011", "graceful") == 0
         assert not TestEnv.is_live(TestEnv.HTTPD_URL, 1)
-        assert self.new_errors() == 1
-        assert self.new_warnings() == 0
+        assert self._new_errors() == 1
+        assert self._new_warnings() == 0
 
     def test_012(self):
         # ManagedDomain does not match any vhost
         assert TestEnv.apachectl("test_012", "graceful") == 0
         assert TestEnv.is_live(TestEnv.HTTPD_URL, 1)
-        assert self.new_errors() == 0
-        assert self.new_warnings() == 1
+        assert self._new_errors() == 0
+        assert self._new_warnings() == 1
 
+    # --------- _utils_ ---------
+
+    def _new_errors(self):
+        (errors, warnings) = TestEnv.apache_err_count()
+        return errors - self.errors
+
+    def _new_warnings(self):
+        (errors, warnings) = TestEnv.apache_err_count()
+        return warnings - self.warnings
