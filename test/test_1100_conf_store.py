4e3a8be0c7a57212fc961993adea397e1a96a488
Michael Köller <michael.koeller@greenbytes.de>
2017-07-04 11:50:28 +0200

    tests: generate valid cert by Makefile

--- test/test_1100_conf_store.py
+++ test/test_1100_conf_store.py
@@ -295,9 +295,9 @@ class TestConf:
         assert TestEnv.a2md([ "update", name, "agreement", TestEnv.ACME_TOS ])['rv'] == 0
         assert TestEnv.is_live(TestEnv.HTTPD_URL, 1)
         assert TestEnv.a2md([ "list", name ])['jout']['output'][0]['state'] == TestEnv.MD_S_INCOMPLETE
-        copyfile(os.path.join(TestEnv.TESTROOT, "data", "ssl", "2017_cert.pem"), TestEnv.path_domain_cert(name))
-        copyfile(os.path.join(TestEnv.TESTROOT, "data", "ssl", "2017_cert.pem"), TestEnv.path_domain_ca_chain(name))
-        copyfile(os.path.join(TestEnv.TESTROOT, "data", "ssl", "2017_pkey.pem"), TestEnv.path_domain_pkey(name))
+        copyfile(os.path.join(TestEnv.TESTROOT, "data", "ssl", "valid_cert.pem"), TestEnv.path_domain_cert(name))
+        copyfile(os.path.join(TestEnv.TESTROOT, "data", "ssl", "valid_cert.pem"), TestEnv.path_domain_ca_chain(name))
+        copyfile(os.path.join(TestEnv.TESTROOT, "data", "ssl", "valid_pkey.pem"), TestEnv.path_domain_pkey(name))
         assert TestEnv.a2md([ "list", name ])['jout']['output'][0]['state'] == TestEnv.MD_S_COMPLETE
         copyfile(os.path.join(TestEnv.TESTROOT, "data", "ssl", "expired_cert.pem"), TestEnv.path_domain_cert(name))
         copyfile(os.path.join(TestEnv.TESTROOT, "data", "ssl", "expired_cert.pem"), TestEnv.path_domain_ca_chain(name))
