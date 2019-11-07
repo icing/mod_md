10200a33f8e1c48b1d007e0d3d83911415556be8
Stefan Eissing <stefan.eissing@greenbytes.de>
2019-11-07 11:52:40 +0100

    * Added a sample scripts/md_message.sh on how to use `MDMessageCmd`.

---
+++ scripts/md_message.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#
+# Sample script to configure for MDMessageCmd.
+# 
+# Edit the USER you want to notify. Comment the "msg=" lines where 
+# you do not want to receive notifications for.
+#
+
+action="$1"
+domain="$2"
+
+USER="webmaster@$domain"
+
+case "$action" in
+    "renewed")
+        subject="renewed $domain certificate"
+        msg="Your Apache renewed the certificate for '$domain'. It will become active after a server reload."
+        ;;
+    "installed")
+        subject="installed $domain certificate"
+        msg="Your Apache installed the certificate for '$domain'. It is now active."
+        ;;
+    "expiring")
+        subject="expiring $domain certificate"
+        msg="Your Apache reports that the certificate for '$domain' will soon expire."
+        ;;
+    "errored")
+        subject="error renewing $domain certificate"
+        msg="There was an error renewing the certificate for '$domain'. Apache will continue trying. Please check the md-status resources or the server log for more information should this repeat."
+        ;;
+    "ocsp-renewed")
+        subject="refreshed OCSP stapling for $domain"
+        msg="The OCSP stapling information for '$domain' was successfully refreshed."
+        ;;
+    "ocsp-errored")
+        subject="error refreshing OCSP stapling for $domain"
+        msg="The was an error refreshing the OCSP stapling information for '$domain'. Apache will continue trying. Please check the md-status resources or the server log for more information should this repeat."
+        ;;
+    *)
+        subject="unknown action in MD message"
+        msg="Your Apache reported action '$action' for domain '$domain'."
+esac
+
+if test "x$msg" = "x"; then exit 0; fi
+
+mail -s "$subject" $USER <<EOF
+$msg
+EOF
